74da1babeeaf428bbc4f76eedc137897c55570e2
Md. Al-Amin <mdalaminh052@gmail.com>
2025-11-23 19:24:28 +0600

    about page added

---
+++ src/app/about-us/page.jsx
@@ -0,0 +1,13 @@
+import MainLayout from '@/layout/MainLayout'
+import About from '@/pages/AboutUs'
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <MainLayout>
+            <About />
+        </MainLayout>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,14 @@
+"us client";
+import AboutCompany from '@/components/AboutUs/AboutCompany'
+import MainLayout from '@/layout/MainLayout';
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <AboutCompany />
+        </div>
+    )
+}
+
+export default About
