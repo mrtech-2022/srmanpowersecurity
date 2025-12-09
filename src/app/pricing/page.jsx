53df7c25e9ce87a479fc3ca9ab4191263f6c0513
mrtech <mrtech.bd2022gmai.com>
2025-12-09 15:27:23 +0600

    supdate

--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -3,7 +3,7 @@ import MainLayout from '@/layout/MainLayout'
 import React from 'react'
 
 export const metadata = {
-    title: 'Pricing - SR Manpower & Security Services',
+    title: 'Pricing - SR Manpower & Security Services.',
     description: 'Explore our pricing options and choose the best plan for your business needs. Our team is dedicated to delivering top-notch solutions that meet your needs.',
 }
 
